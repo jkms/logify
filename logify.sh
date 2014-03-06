a8178463039830620ea6369929e9b0a95cdbc6c2
John <github@jkms.me>
2014-03-06 11:23:34 -0800

    thumbnail cleanup

--- logify.sh
+++ logify.sh
@@ -2,8 +2,20 @@
 # renames and organizes personal log files
 
 LOGFILE=$1
+THUMBEXT=THM
+THUMBFILE=${LOGFILE%.*}.$THUMBEXT
 FILEDATE=$(date +"%Y%m%d%H%M" -r $LOGFILE)
 LOGPATH=~/Videos/Log
 
+
 mv $LOGFILE $LOGPATH/$FILEDATE.${LOGFILE##*.}
+
+if [ -f $THUMBFILE ];
+then
+	rm $THUMBFILE
+	echo cleaned up $THUMBFILE
+else
+	echo $THUMBFILE not found
+fi
+
 echo moved $LOGFILE to $LOGPATH/$FILEDATE.${LOGFILE##*.}
