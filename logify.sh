18c351c49dec30ab55bee67b7ad5b066b210310e
John <john@jkms.me>
2014-03-07 11:42:42 -0800

    Implemented logification of entire folders

--- logify.sh
+++ logify.sh
@@ -1,33 +1,54 @@
 #!/bin/sh
 # renames and organizes personal log files
 
-LOGFILE=$1
-LOGEXT=${LOGFILE##*.}
-THUMBEXT=THM
-THUMBFILE=${LOGFILE%.*}.$THUMBEXT
-FILEDATE=$(date +"%Y%m%d%H%M" -r $LOGFILE)
 LOGPATH=~/Videos/Log
-DESTFILE=$LOGPATH/$FILEDATE.$LOGEXT
 
-mv $LOGFILE $DESTFILE
-if [ -f $THUMBFILE ];
-then
-  rm $THUMBFILE
-  echo cleaned up $THUMBFILE
+movelog() {
+	LOGFILE=$1
+	FILEDATE=$(date +"%Y%m%d%H%M" -r $LOGFILE)
+	LOGEXT=${LOGFILE##*.}
+	DESTFILE=$LOGPATH/$FILEDATE.$LOGEXT	
+	THUMBEXT=THM
+	THUMBFILE=${LOGFILE%.*}.$THUMBEXT
+
+	if [ "$LOGEXT" = "MOV" ] || [ "$LOGEXT" = "mov" ] || [ "$LOGEXT" = "AVI" ] || [ "$LOGEXT" = "avi" ];then
+
+		mv $LOGFILE $DESTFILE
+		if [ -f $THUMBFILE ]; then
+			rm $THUMBFILE
+			echo cleaned up $THUMBFILE
+#		else
+#			echo $THUMBFILE not found
+		fi
+
+		echo "moved $LOGFILE to $DESTFILE"
+
+		echo "Renencoding file to OGV..."
+		ffmpeg2theora $DESTFILE > /dev/null 2>&1
+		echo "Reencode complete!"
+
+		OUT=$?
+		if [ $OUT -eq 0 ]; then
+			echo "lookin' good, cleaning up source $DESTFILE"
+			trash $DESTFILE
+		else
+			echo "Something weird happened, ffmpeg2theora exit status $out. Please check files"
+		fi
+	fi
+}
+
+if [ -d "$1" ]; then
+	INPUTFILES=$1/*
+	for f in $INPUTFILES; do
+		if [ ${f##*.} = MOV ]; then
+			movelog $f
+		else
+			echo "not an appropriate file type"
+		fi
+	done
 else
-  echo $THUMBFILE not found
+	movelog $1
 fi
 
-echo "moved $LOGFILE to $DESTFILE"
-
-echo "I want to cmpare $LOGEXT to MOV"
-if [ "$LOGEXT" = "MOV" ] || [ "$LOGEXT" = "mov" ] || [ "$LOGEXT" = "AVI" ] || [ "$LOGEXT" = "avi" ];then
-  ffmpeg2theora $DESTFILE 
-  OUT=$?
-  if [ $OUT -eq 0 ];then
-    echo "lookin' good, cleanup old files"
-    trash $DESTFILE
-  else
-    echo "Something weird happened, ffmpeg2theora exit status $out. Please check files"
-  fi
-fi
+echo
+echo "all done!"
